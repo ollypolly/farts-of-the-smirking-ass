e93f3b6dfe5883400780e431a99349bfd8f4aca7
Olly Pethick <ollypethick@gmail.com>
2025-03-18 16:08:50 +0000

    Add toilet roll component

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -1,4 +1,3 @@
-import { Canvas } from '@react-three/fiber'
 import Container from 'components/BlogContainer'
 import BlogHeader from 'components/BlogHeader'
 import Layout from 'components/BlogLayout'
@@ -7,6 +6,7 @@ import IndexPageHead from 'components/IndexPageHead'
 import MoreStories from 'components/MoreStories'
 import * as demo from 'lib/demo.data'
 import type { Post, Settings } from 'lib/sanity.queries'
+import { ToiletRoll } from './ToiletRoll'
 
 export interface IndexPageProps {
   preview?: boolean
@@ -26,26 +26,8 @@ export default function IndexPage(props: IndexPageProps) {
 
       <Layout preview={preview} loading={loading}>
         <Container>
-          <Canvas>
-            <mesh>
-              <boxGeometry args={[2, 2, 2]} />
-              <meshPhongMaterial />
-            </mesh>
-            <ambientLight intensity={0.1} />
-            <directionalLight position={[0, 0, 5]} color="red" />
-          </Canvas>
           <BlogHeader title={title} description={description} level={1} />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <ToiletRoll />
         </Container>
       </Layout>
     </>
