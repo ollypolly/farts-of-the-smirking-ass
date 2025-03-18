e4669730dcccd7c38a3cfdd7a61bef4c8e821768
Olly Pethick <ollypethick@gmail.com>
2025-03-18 16:05:30 +0000

    Install react three fiber

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -7,26 +7,14 @@
  * https://portabletext.org/
  *
  */
-import {
-  PortableText,
-  type PortableTextReactComponents,
-} from '@portabletext/react'
+import { PortableText } from '@portabletext/react'
 
 import styles from './PostBody.module.css'
-import { SanityImage } from './SanityImage'
-
-const myPortableTextComponents: Partial<PortableTextReactComponents> = {
-  types: {
-    image: ({ value }) => {
-      return <SanityImage {...value} />
-    },
-  },
-}
 
 export default function PostBody({ content }) {
   return (
     <div className={`mx-auto max-w-2xl ${styles.portableText}`}>
-      <PortableText value={content} components={myPortableTextComponents} />
+      <PortableText value={content} />
     </div>
   )
 }
