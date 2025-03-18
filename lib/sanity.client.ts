380282b9511cb484dd299d24dd45628188679a3b
Olly Pethick <ollypethick@gmail.com>
2025-03-18 15:57:06 +0000

    Update deps

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -23,8 +23,9 @@ export function getClient(preview?: { token: string }): SanityClient {
     apiVersion,
     useCdn,
     perspective: 'published',
-    encodeSourceMap: preview?.token ? true : false,
-    studioUrl,
+    stega: {
+      enabled: preview?.token ? true : false,
+    },
   })
   if (preview) {
     if (!preview.token) {
